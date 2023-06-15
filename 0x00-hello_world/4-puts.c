7da5a8cfc8467d32d2df3fe64e9788b06bfb3d69
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-15 13:18:00 -0700

    a c-program that prints specifics

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - printing without using the printf function
+*
+*Return: 0 Success
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
