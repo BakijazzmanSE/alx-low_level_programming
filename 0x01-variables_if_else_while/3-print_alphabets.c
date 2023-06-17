959541e60e316ae4479f601d84098ff99fe71382
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-16 21:43:50 -0700

    print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
-  2 /**
-  3  * main - prints alpabeths in upper and lover case
-  4  *
-  5  * Return: 0 success
-  6  */
-  7 int main(void)
-  8 {
-  9         char alpha = 'a';
- 10         char ALPHA = 'A';
- 11
- 12         while (alpha <= 'z')
- 13         {
- 14                 putchar(alpha);
- 15                 alpha++;
- 16         }
- 17         while (ALPHA <= 'Z')
- 18         {
- 19                 putchar(ALPHA);
- 20                 ALPHA++;
- 21         }
- 22         putchar('\n');
- 23
- 24         return (0);
- 25 }
+/**
+ * main - prints alpabeths in upper and lover case
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+        char alpha = 'a';
+        char ALPHA = 'A';
+
+        while (alpha <= 'z')
+        {
+                putchar(alpha);
+                alpha++;
+        }
+        while (ALPHA <= 'Z')
+        {
+                putchar(ALPHA);
+                ALPHA++;
+        }
+        putchar('\n');
+
+        return (0);
 }
