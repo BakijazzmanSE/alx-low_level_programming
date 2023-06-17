db9bd4afeb27a1dd675fde377725ff1378bd0e83
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-16 21:47:31 -0700

    betty

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,20 +6,20 @@
  */
 int main(void)
 {
-        char alpha = 'a';
-        char ALPHA = 'A';
+	char alpha = 'a';
+	char ALPHA = 'A';
 
-        while (alpha <= 'z')
-        {
-                putchar(alpha);
-                alpha++;
-        }
-        while (ALPHA <= 'Z')
-        {
-                putchar(ALPHA);
-                ALPHA++;
-        }
-        putchar('\n');
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	while (ALPHA <= 'Z')
+	{
+		putchar(ALPHA);
+		ALPHA++;
+	}
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
