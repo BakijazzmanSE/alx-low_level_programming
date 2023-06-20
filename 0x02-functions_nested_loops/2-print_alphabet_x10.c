c71a2298a49f2bf29afdd8c4295ccd69f40cf9e5
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-20 13:53:57 -0700

    alphabet 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	int times10;
 	char alpha;
 
-	for(times10 = 0; times10 < 10; times10++)
+	for (times10 = 0; times10 < 10; times10++)
 	{
 		alpha = 'a';
 		while (alpha <= 'z')
