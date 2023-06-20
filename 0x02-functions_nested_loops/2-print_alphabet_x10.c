1594f01e951d7e2f6e5ba3bef5bd3e0fe1e25492
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-20 13:49:37 -0700

    print 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets *10
+ *
+ * Return: 0 success
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int times10;
+	char alpha;
+
+	for(times10 = 0; times10 < 10; times10++)
+	{
+		alpha = 'a';
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+	}
+}
