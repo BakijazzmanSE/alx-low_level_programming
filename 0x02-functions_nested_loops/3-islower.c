354716fd54e7d38423827fc119137530df8bf139
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-20 15:06:11 -0700

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _islower - checks lowercase character
- *
- * Return: 1 if c is lowercase
+ *@c: is the character to be checked
+ * Return: c is lowercase
  *
  */
 int _islower(int c)
