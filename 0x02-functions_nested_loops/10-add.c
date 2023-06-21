b92e768e6ffef8ac63a4b256632a0f43b44c0382
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-20 17:53:16 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add two integers and returns the result
+ * Return: result
+ * @num1: first number
+ * @num2: second number
+ */
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+	return (result);
+}
