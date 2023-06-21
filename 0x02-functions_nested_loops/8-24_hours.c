a0d4ce302b01ac895be209305c6a8aa95bcc55e7
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-20 17:09:44 -0700

    tell the time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minutes of the day
+ * Return : void
+ *
+ */
+void jack_bauer(void)
+{
+	int n, j;
+
+	for (j = 0; j <= 23; j++)
+	{
+		for (n = 0; n <= 59; n++)
+		{
+			_putchar((j / 10) + '0');
+			_putchar((j % 10)  + '0');
+			_putchar(':');
+			_putchar((n / 10) + '0');
+	       		_putchar((n % 10) + '0');
+			_putchar('\n');
+	
+		}
+	}
+}
