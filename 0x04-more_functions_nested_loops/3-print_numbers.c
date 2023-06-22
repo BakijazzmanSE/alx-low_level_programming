8c2abd0cbca70a8300960362a2ff26ccef34f421
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-22 15:58:55 -0700

    betty issue solved

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * print_numbers - a function that prints numbers from 0-9
- * followe by a new line 
+ * followe by a new line
  * Return: void
  */
 void print_numbers(void)
 {
 	int num;
 
-	for(num = 0; num <= 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar(num + '0');
 	}
