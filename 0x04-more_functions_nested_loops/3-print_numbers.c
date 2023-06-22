dea91fa698dcd3856ca4c9735fd787444b26e44a
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-22 15:56:58 -0700

    primnt numbers from zero to nine

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - a function that prints numbers from 0-9
+ * followe by a new line 
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for(num = 0; num <= 9; num++)
+	{
+		_putchar(num + '0');
+	}
+	_putchar('\n');
+}
