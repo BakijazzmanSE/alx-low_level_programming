5cb29f20c91b51533847b9ea495bd59c5af70873
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-22 16:40:11 -0700

    betty issue fixed

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_line - function that draws a straight line on the terminal
- * @n: the number of times the _ should be printed 
+ * @n: the number of times the _ should be printed
  * Return: nothing
  */
 void print_line(int n)
