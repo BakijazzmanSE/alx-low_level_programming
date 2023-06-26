72cd703af3ade00c02faea51925ff4b1c685d115
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-26 06:26:23 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value of parameter to 98
+ * Return: nothing
+ * @*n: the parameter to be updated
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
