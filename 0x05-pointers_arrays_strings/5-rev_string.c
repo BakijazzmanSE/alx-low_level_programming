21b22cb0819b7ce18cd830a2c1f62a4098cc7f5b
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-26 13:41:33 -0700

    ev sting

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,28 @@
 #include "main.h"
+
 /**
- * rev_string -function that reveses a string
- * @s: the parameter
- * Return: nothing
+ * rev_string - function that prints a string, in reverse
+ * @s: String to print
+ * Return: nothing.
  */
+
 void rev_string(char *s)
 {
-	int i = 0;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	i = i - 1;
-	while (i >= 0)
-	{
-		_putchar(s[i]);
-		i--;
-	}
+int i = 0;
+int aux = 0;
+char ltemp;
+
+while (*(s + i) != '\0')
+i += 1;
+i -= 1;
+
+while (aux < i)
+{
+ltemp = s[i];
+s[i] = s[aux];
+s[aux] = ltemp;
+aux++;
+i--;
+}
 }
