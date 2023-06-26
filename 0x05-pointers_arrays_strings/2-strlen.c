845cbb2d3f8ebbf92005126d4638813b91cb2400
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-26 07:30:23 -0700

    strring lengtth

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - returns the lenght of a string
+ * @s: the parameter
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = strlen(s);
+
+	return (i);
+}
