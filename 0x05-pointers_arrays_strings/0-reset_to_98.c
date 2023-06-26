577d7e7bddf56d9a28e00bee215d6bbe53dccc24
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-26 06:27:54 -0700

    betty add

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - updates the value of parameter to 98
  * Return: nothing
- * @*n: the parameter to be updated
+ * @n: the parameter to be updated
  */
 void reset_to_98(int *n)
 {
