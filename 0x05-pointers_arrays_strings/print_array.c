08f99562edb73990d3f9d7eba7d470248493d304
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-27 14:59:49 -0700

    print array

---
+++ 0x05-pointers_arrays_strings/print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_array - function that prints elements of an array
+ * @a: the array
+ * @n: number of elements
+ */
+void print_array(int *a, int n)
+{
+	int b, i = 0;
+
+	while (a[i] < n)
+	{
+		b = n - 1;
+		printf("%d, ", a[i])
+		i++;
+		if (i == b)
+		{
+			printf("%d", a[b])
+		}
+	}
+	printf("\n");
+}
