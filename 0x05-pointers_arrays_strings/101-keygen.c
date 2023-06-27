b11583c971881f2be11c52c77bf68f7562f42436
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-27 15:14:47 -0700

    password generator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#define MIN_LENGTH 8
+#define MAX_LENGTH 12
+
+char* generate_password() 
+{
+    int i;
+    char* password = malloc(MAX_LENGTH + 1);
+    int password_length = rand() % (MAX_LENGTH - MIN_LENGTH + 1) + MIN_LENGTH;
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    srand(time(NULL));
+  
+    for (i = 0; i < password_length; i++)
+    {
+        int index = rand() % strlen(charset);
+      
+        password[i] = charset[index];
+    }
+    password[password_length] = '\0';
+    return password;
+}
+
+
+int main() {
+    char* password = generate_password();
+    printf("%s\n", password);
+    free(password);
+    return 0;
+}
