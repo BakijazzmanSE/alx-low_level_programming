587d003f86a07965b3b3791caa732d55b8b86120
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-27 15:04:34 -0700

    missing include fixed

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_array - function that prints elements of an array
  * @a: the array
@@ -11,11 +12,11 @@ void print_array(int *a, int n)
 	while (a[i] < n)
 	{
 		b = n - 1;
-		printf("%d, ", a[i])
+		printf("%d, ", a[i]);
 		i++;
 		if (i == b)
 		{
-			printf("%d", a[b])
+			printf("%d", a[b]);
 		}
 	}
 	printf("\n");
