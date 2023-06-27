027f64c9067a0413260a88b9e246ff2dab3d8850
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-27 14:41:04 -0700

    printing half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,12 +7,26 @@ void puts_half(char *str)
 {
 	int i, n;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 0)
 	{
 		n = i / 2;
-		while (str[n] < i)
+		while (str[n] != '\0')
 		{
 			_putchar(str[n]);
+			n++;
+		}
+	}
+	else
+	{
+		n = i - 1;
+		while (str[n] != '\0')
+		{
+			_putchar(str[n]);
+			n++;
 		}
 	}
 	_putchar('\n');
