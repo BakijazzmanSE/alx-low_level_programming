2b55efbb5bab1ac172ccefbf98b490a3c1324b22
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-06-30 15:43:32 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,15 +11,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-		int counter, comVal;
+	int counter, comVal;
 
-			counter = 0;
-
-				while (s1[counter] == s2[counter] && s1[counter] != '\0')
-						{
-									counter++;
-										}
-
-					comVal = s1[counter] - s2[counter];
-						return (comVal);
+	counter = 0;
+	while (s1[counter] == s2[counter] && s1[counter] != '\0')
+	{
+		counter++;
+	}
+	comVal = s1[counter] - s2[counter];
+	return (comVal);
 }
