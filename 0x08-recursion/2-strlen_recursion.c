04dcfc001e1e0014c6dcfc698b249b86055cbec7
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-07-05 14:28:32 -0700

    length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strlen_recursion - length of a string
+ * @s: string
+ * Return: int length
+ */
+
+int _strlen_recursion(char *s)
+{
+	int result;
+
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		result = _strlen_recursion(s);
+		result++;
+		return (result);
+	}
+}
