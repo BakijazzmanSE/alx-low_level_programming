0bb2af3317c66b470298f3742fe0e9f4d38318fb
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-07-07 07:48:54 -0700

    static and dynamic lirary

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower - checks lowercase character
+ *@c: is the character to be checked
+ * Return: c is lowercase
+ *
+ */
+int _islower(int c)
+{
+	return (c <= 'z' && c >= 'a');
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * Return: 0 success
+ * @num: the absolute value
+ */
+int _abs(int num)
+{
+	if (num > 0)
+	{
+		return (num);
+	}
+
+	return (-num);
+}
