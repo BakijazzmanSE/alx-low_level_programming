ebb5fef84289cb027f03f567fcfa54a444508a99
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-07-17 11:54:59 -0700

    intialize dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * init_dog - function that initiates the variable of type struct
+ * @d: the pointer to the struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		char *name = name;
+		float age = age;
+		char *owner = owner;
+	}
+}
