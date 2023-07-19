622721ae49232184be6382171dcfabbcab0f6330
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-07-19 16:25:51 -0700

    iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * int_index - function that executes a functiongiven as a parameter
+ * @size: the size of the array
+ * @array: the array of functions
+ * @action: the function to be performed
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	 unsigned int i = 0;
+
+	 if (action && array)
+	 while (i < size)
+	 {
+	 	action(array[i]);
+		i++;
+	 }
+}
