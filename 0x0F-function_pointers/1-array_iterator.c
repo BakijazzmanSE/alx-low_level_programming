cf44bd44c7aef26ecd6ae2a7c216206538c43d91
Obafemi Olayinka _BakijazzmanSE_ <obafemipro@outlook.com>
2023-07-19 16:34:22 -0700

    misspelled word

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,10 +9,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	 unsigned int i = 0;
+	unsigned int i = 0;
 
-	 if (acion && array)
-	 while (i < size)
+	if (action && array)
+	while (i < size)
 	 {
 	 	action(array[i]);
 		i++;
